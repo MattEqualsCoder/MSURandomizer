fe4ec5208c1cbcba82bde3a061a6d86cf635ea7b
MattEqualsCoder <mattequalscoder@gmail.com>
2024-07-22 11:54:49 -0400

    Reset version number and remove legacy code

--- Setup/CopyNugetPackages.ps1
+++ Setup/CopyNugetPackages.ps1
@@ -1,4 +1,3 @@
 Set-Location $PSScriptRoot
 Copy-Item -Path ..\MSURandomizerLibrary\bin\Release\*.nupkg -Destination $PSScriptRoot\Output
-Copy-Item -Path ..\MSURandomizerUI\bin\Release\*.nupkg -Destination $PSScriptRoot\Output
 Copy-Item -Path ..\MSURandomizer\bin\Release\*.nupkg -Destination $PSScriptRoot\Output
